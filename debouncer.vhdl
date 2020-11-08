5c7529fe375ecec98ccb2f5172c43183f842a262
Dylan Richards <dylan.richards81@gmail.com>
2020-11-08 15:40:27 -0500

    Updated debouncer

--- debouncer.vhdl
+++ debouncer.vhdl
@@ -5,29 +5,40 @@
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY debouncer IS
     PORT (
         D : IN STD_LOGIC;
         CLK : IN STD_LOGIC; -- 100 MHz E3
-        Q : OUT STD_LOGIC
+        Q : OUT STD_LOGIC := '0'
     );
 END debouncer;
 
 ARCHITECTURE Behavioral OF debouncer IS
-    SIGNAL five_clock_counter : STD_LOGIC_VECTOR (2 DOWNTO 0);
+    SIGNAL five_clock_counter : STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
 BEGIN
 
     PROCESS (CLK)
     BEGIN
-        IF (rising_edge(CLK) AND D = '1') THEN
-            IF (five_clock_counter < "101") THEN
-                five_clock_counter <= five_clock_counter + 1;
+        IF(rising_edge(CLK)) THEN 
+            IF (D = '1') THEN
+                IF (five_clock_counter < x"4") THEN
+                    five_clock_counter <= five_clock_counter + x"1";
+                ELSE
+                    five_clock_counter <= five_clock_counter - x"2";
+                    Q <= '1';
+                END IF;
             ELSE
-                five_clock_counter = (OTHERS => '0');
-                Q <= '1';
+                IF (five_clock_counter < x"5") THEN
+                    five_clock_counter <= five_clock_counter + x"1";
+                ELSE
+                    five_clock_counter <= (OTHERS => '0');
+                    Q <= '0';
+                END IF;
             END IF;
         END IF;
+        
     END PROCESS;
 
 END Behavioral;
